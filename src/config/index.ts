3ff1f98a04dc7feb500be73eab386c27ab77f8f8
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-19 00:38:43 +0600

    feat: add database URL to configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,7 +4,8 @@ import { env } from 'process'
 dotenv.config()
 
 const config={
-    port:env.PORT
+    port:env.PORT,
+    database_url:env.DATABASE_URL
 }
 
 export default config
--- src/server.ts
+++ src/server.ts
@@ -6,6 +6,7 @@ import config from "./config"
 
 
 const main = async () => {
+    // console.log(config.database)
     app.listen(config.port, () => {
 console.log(`server is raining ${config.port}`)
     })
