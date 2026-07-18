e11419d66b48a2e369417d8c9f994e74dc102dcd
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-19 00:53:56 +0600

    feat: add PostgreSQL support with Neon and update configuration

--- src/config/index.ts
+++ src/config/index.ts
@@ -4,8 +4,8 @@ import { env } from 'process'
 dotenv.config()
 
 const config={
-    port:env.PORT,
-    database_url:env.DATABASE_URL
+    port:env.PORT as string,
+    database_url:env.DATABASE_URL as string
 }
 
 export default config
---
+++ src/db/index.ts
@@ -0,0 +1,12 @@
+import {neon} from "@neondatabase/serverless"
+import config from "../config"
+
+export const sql = neon(config.database_url)
+
+//when i used pg that
+
+// import { Pool } from "pg";
+
+// export const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+// });
