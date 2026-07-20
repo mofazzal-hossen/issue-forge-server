ae7e4b6ae053426425af08f90de11e4458506fbc
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-20 23:24:36 +0600

    feat: add user insertion query and update database interaction

--- src/db/index.ts
+++ src/db/index.ts
@@ -1,4 +1,4 @@
-import {neon} from "@neondatabase/serverless"
+import {neon, } from "@neondatabase/serverless"
 import config from "../config"
 
 export const sql = neon(config.database_url)
@@ -10,3 +10,15 @@ export const sql = neon(config.database_url)
 // export const pool = new Pool({
 //   connectionString: process.env.DATABASE_URL,
 // });
+
+const query = `
+  INSERT INTO users (name, email, password)
+  VALUES ($1, $2, $3)
+  RETURNING *;
+`;
+
+const values = ["Munna", "munna@gmail.com", "123456"];
+
+const result = await Pool.query(query, values);
+
+console.log(result.rows[0]);
