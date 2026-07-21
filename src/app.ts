85758af7cb1de4ceedd57ac5e87c825d53e7fb79
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-21 23:41:47 +0600

    feat: initialize database connection in app and clean up db module

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { type Request, type Response } from 'express'
+import { initDB } from './db'
 
 
 const app = express()
+initDB()
 app.get('/', (req:Request, res:Response) => {
   res.send('Hello World!')
 })
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,13 +1,12 @@
-import {neon, } from "@neondatabase/serverless"
+import { neon, } from "@neondatabase/serverless"
 import config from "../config"
 
 export const sql = neon(config.database_url)
 
 //when i used pg that
 
-// import { Pool } from "pg";
 
-// export const pool = new Pool({
-//   connectionString: process.env.DATABASE_URL,
-// });
 
+export const initDB = async () => {
+    console.log("Data DB Connect ")
+}
