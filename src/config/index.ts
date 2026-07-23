988353e12bd0dca245d5db2613e1aede831c9729
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-23 23:26:15 +0600

    fix: enable error throwing in root route and improve error handling stack trace in development

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,9 +3,10 @@ import dotenv from 'dotenv'
 import { env } from 'process'
 dotenv.config()
 
-const config={
-    port:env.PORT as string,
-    database_url:env.DATABASE_URL as string
+const config = {
+    port: env.PORT as string,
+    database_url: env.DATABASE_URL as string,
+    node_env: env.NODE_ENV as string
 }
 
 export default config
--- src/middleware/globalErrorHandler.ts
+++ src/middleware/globalErrorHandler.ts
@@ -1,11 +1,14 @@
 
+
 import type { NextFunction, Request, Response } from "express";
+import config from "../config";
 
 export const globalErrorHandler = (err:unknown,req:Request, res:Response, next:NextFunction) => {
   
     res.status(500).json({
         success:false,
         message:err instanceof Error? err.message: "Internal Server Error" ,
-        stack: err.stack
+
+        stack: config.node_env === "development" && err instanceof Error? err.message: undefined
     })
 }
