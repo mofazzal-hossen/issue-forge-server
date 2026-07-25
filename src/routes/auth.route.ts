ba5d4b8e73b1d845667bf967a551c835aa9b0d64
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-25 22:55:49 +0600

    fix: correct route definitions for user authentication

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -3,5 +3,11 @@ import { Router } from "express";
 const router  = Router()
 
 router.post('sinup',()=>{})
+
 router.post('/sinin',()=>{})
-router.post('/me',()=>{})
+
+router.get('/me',()=>{})
+
+router.put('/update/:id',()=>{})
+
+router.delete('/delete/:id',()=>{})
