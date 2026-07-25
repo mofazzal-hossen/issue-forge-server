7705c0ad470e2bc636142e688452f68d725a104e
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-07-25 23:15:27 +0600

    feat: add bcrypt for password hashing and implement createUser method in AuthService

---
+++ src/service/auth.route.ts
@@ -0,0 +1,30 @@
+import { sql } from "../db";
+import type { RUser } from "../types";
+import bcrypt from "bcrypt"
+
+class AuthService {
+    async createUser(user: RUser & { password: string }) {
+        const { name, email, age, role, password } = user;
+
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        const res = await sql`
+      INSERT INTO users (name, email, passwordHash, age, role)
+      VALUES (
+        ${name},
+        ${email},
+        ${passwordHash},
+        ${age},
+        COALESCE(${role}, 'user')
+      )
+       RETURNING id , name , age, role
+    `;
+
+        return res[0];
+    }
+}
+
+export default new AuthService()
+
+
+//   RETURNING *;
