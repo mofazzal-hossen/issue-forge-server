59d39bc022fc9a242415a56ec1291eac01d80040
mofazzal-hossen <mofazzal.hossen.official0@gmail.com>
2026-08-01 23:31:25 +0600

    feat: update token signing function to use config secret and set expiration

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -1,14 +1,16 @@
+import config from "../config";
 import type { RUser } from "../types";
 import jwt from 'jsonwebtoken'
 
 const singToken= (payload: RUser)=>{
-const accessToken = jwt.sign(payload,"super",{
-    expiresIn:''
+const accessToken = jwt.sign(payload,config.access_secret,{
+    expiresIn:'1d'
 })
-
+return accessToken
 
 }
 
+console.log( singToken({age:123, email:"hohi@gmail.com", name: "rest", role:"admin"}))
 
 
 
